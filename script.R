50ef867e5e55fb338839a06b114914bd144164cc
Roberto Salas <wala.rsalas@gmail.com>
2018-09-28 18:31:25 -0300

    updated keep parameter and added more geojson files

--- script.R
+++ script.R
@@ -4,22 +4,25 @@ library(rmapshaper)
 
 simplygeojson <- function(x, name, keep){
   shp <- ms_simplify(x, keep=keep)
-  write_sf(shp, dsn = paste0(name, "/","R", sprintf("%02d", as.numeric(unique(x$REGION))),".geojson"), delete_dsn = FALSE)
+  write_sf(shp, dsn = paste0(name, "/","R", sprintf("%02d", as.numeric(unique(x$REGION))),".geojson"), delete_dsn = FALSE, overwrite=TRUE)
 }
 
 data1 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Comuna.shp"))})
-lapply(data1, function(x) {simplygeojson(x, "Comunas", 0.01)})
+lapply(data1, function(x) {simplygeojson(x, "Comunas", 0.2)})
 
-#data2 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Provincia.shp"))})
-#lapply(data2, function(x) {simplygeojson(x, "Provincias")})
-#
-#data3 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Manzana_Precensal.shp"))})
-#lapply(data3, function(x) {simplygeojson(x, "Manzanas")})
-#
-#data4 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Region.shp"))})
-#lapply(data4, function(x) {simplygeojson(x, "Regiones")})
-#
-#data5 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Distrito_Censal.shp"))})
-#lapply(data5, function(x) {simplygeojson(x, "Distritos")})
+data2 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Limite_Urbano_Censal.shp"))})
+lapply(data2, function(x) {simplygeojson(x, "Limite_Urbano", 0.2)})
+
+data3 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Provincia.shp"))})
+lapply(data3, function(x) {simplygeojson(x, "Provincias", 0.2)})
+
+data4 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Manzana_Precensal.shp"))})
+lapply(data4, function(x) {simplygeojson(x, "Manzanas", 0.2)})
+
+data5 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Region.shp"))})
+lapply(data5, function(x) {simplygeojson(x, "Regiones", 0.2)})
+
+data6 <- lapply(paste0("R",sprintf("%02d",1:15)), function(x){read_sf(paste0("/Users/robsalasco/Documents/CENSO2017/Cartografia/",x,"/Distrito_Censal.shp"))})
+lapply(data6, function(x) {simplygeojson(x, "Distritos", 0.2)})
 
 
